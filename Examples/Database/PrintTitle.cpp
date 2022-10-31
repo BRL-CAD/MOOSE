52e7e60f6800ee5eef32fdca04f228fffb6ce7bb
Daniel Rossberg <33295041+drossberg@users.noreply.github.com>
2022-10-31 19:40:39 +0100

    a first set of examples
    - example CMake config for MOOSE applications
    - basic database example
    - basic command string example

---
+++ Examples/Database/PrintTitle.cpp
@@ -0,0 +1,42 @@
+/*
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <iostream>
+
+#include <brlcad/Database/ConstDatabase.h>
+
+
+int main
+(
+    int   argc,
+    char* argv[]
+) {
+    int ret = 0;
+
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <BRL-CAD Database>";
+        ret = 1;
+    }
+    else {
+        BRLCAD::ConstDatabase database;
+
+        if (database.Load(argv[1]))
+            std::cout << database.Title() << std::endl;
+        else {
+            std::cerr << "Could not load file: " << argv[1] << std::endl;
+            ret = 2;
+        }
+    }
+
+    return ret;
+}
