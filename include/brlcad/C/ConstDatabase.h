1799ed542c4102b9e9d8af20b8b7d08092186b9b
Daniel Rossberg <33295041+drossberg@users.noreply.github.com>
2026-05-08 14:33:48 +0200

    drafted an idea regarding a simplified C API

---
+++ include/brlcad/C/ConstDatabase.h
@@ -0,0 +1,48 @@
+/*                      C O N S T D A T A B A S E . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2026 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file ConstDatabase.h
+ *
+ *  BRL-CAD core simplified C interface:
+ *      declares a handle and functions for a read-only database
+ */
+
+#ifndef BRLCAD_C_CONSTDATABASE_INCLUDED
+#define BRLCAD_C_CONSTDATABASE_INCLUDED
+
+
+extern "C" {
+
+
+typedef void* BrlConstDatabase;
+
+
+BRLCAD_MOOSE_EXPORT BrlConstDatabase BrlNewConstDatabase(void);
+BRLCAD_MOOSE_EXPORT void             BrlDeleteConstDatabase(BrlConstDatabase db);
+
+BRLCAD_MOOSE_EXPORT int              BrlConstDatabaseLoad(BrlConstDatabase db,
+                                                          const char*      fileName);
+
+BRLCAD_MOOSE_EXPORT const char*      BrlConstDatabaseTitle(BrlConstDatabase db);
+
+
+}
+
+
+#endif // BRLCAD_C_CONSTDATABASE_INCLUDED
---
+++ src/C/ConstDatabase.cpp
@@ -0,0 +1,91 @@
+/*                      C O N S T D A T A B A S E . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2026 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file ConstDatabase.cpp
+ *
+ *  BRL-CAD core simplified C interface:
+ *      implements a handle and functions for a read-only database
+ */
+
+#include <cassert>
+
+#include <brlcad/Database/ConstDatabase.h>
+#include <brlcad/C/ConstDatabase.h>
+
+
+using namespace BRLCAD;
+
+
+BrlConstDatabase BrlNewConstDatabase(void) {
+    return new ConstDatabase;
+}
+
+
+void BrlDeleteConstDatabase
+(
+    BrlConstDatabase db
+) {
+    if (db != nullptr) {
+        ConstDatabase* constDatabase = dynamic_cast<ConstDatabase*>(db);
+
+        assert(constDatabase != nullptr);
+
+        if (constDatabase != nullptr)
+            delete constDatabase;
+    }
+}
+
+
+int BrlConstDatabaseLoad
+(
+    BrlConstDatabase db,
+    const char*      fileName
+) {
+    int ret = 1;
+
+    if (db != nullptr) {
+        ConstDatabase* constDatabase = dynamic_cast<ConstDatabase*>(db);
+
+        assert(constDatabase != nullptr);
+
+        if (constDatabase != nullptr)
+            ret = constDatabase->Load(fileName) ? 0 : 1;
+    }
+
+    return ret;
+}
+
+
+const char* BrlConstDatabaseTitle
+(
+    BrlConstDatabase db
+) {
+    const char* ret = nullptr;
+
+    if (db != nullptr) {
+        ConstDatabase* constDatabase = dynamic_cast<ConstDatabase*>(db);
+
+        assert(constDatabase != nullptr);
+
+        if (constDatabase != nullptr)
+            ret = constDatabase->Title(fileName);
+    }
+
+    return ret;
+}
