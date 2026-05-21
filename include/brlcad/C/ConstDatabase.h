1f85c486694ef0205a26dbd1997b1c95ba9c3046
waleed <iwaleedahmed05@gmail.com>
2026-05-21 08:09:34 +0500

    retrieved ConstDatabase.h

--- include/brlcad/C/ConstDatabase.h
+++ include/brlcad/C/ConstDatabase.h
@@ -36,10 +36,6 @@ typedef void* BrlConstDatabase;
 
 BRLCAD_MOOSE_EXPORT BrlConstDatabase BrlNewConstDatabase(void);
 
-BRLCAD_MOOSE_EXPORT BrlConstDatabase BrlNewFileDatabase(void);
-
-BRLCAD_MOOSE_EXPORT BrlConstDatabase BrlNewMemoryDatabase(void);
-
 BRLCAD_MOOSE_EXPORT void             BrlDeleteConstDatabase(BrlConstDatabase db);
 
 BRLCAD_MOOSE_EXPORT int              BrlConstDatabaseLoad(BrlConstDatabase db,
