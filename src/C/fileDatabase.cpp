0f1843a0331b1b9bdb8b5d4a30294a9ba0967d5a
Daniel Rossberg <33295041+drossberg@users.noreply.github.com>
2026-05-23 18:19:17 +0200

    constructors for file and in-memory databases

---
+++ src/C/fileDatabase.cpp
@@ -0,0 +1,37 @@
+/*                      F I L E D A T A B A S E . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2026 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file fileDatabase.cpp
+ *
+ *  BRL-CAD core simplified C interface:
+ *      implements a handle and functions for a read-write file based
+ *      database
+ */
+
+#include <brlcad/Database/FileDatabase.h>
+
+#include <brlcad/C/fileDatabase.h>
+
+
+using namespace BRLCAD;
+
+
+BrlFileDatabase BrlNewFileDatabase(void) {
+    return new FileDatabase;
+}
---
+++ src/C/memoryDatabase.cpp
@@ -0,0 +1,36 @@
+/*                  M E M O R Y D A T A B A S E . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2026 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file memoryDatabase.cpp
+ *
+ *  BRL-CAD core simplified C interface:
+ *      implements a handle and functions for an in-memory database
+ */
+
+#include <brlcad/Database/MemoryDatabase.h>
+
+#include <brlcad/C/memoryDatabase.h>
+
+
+using namespace BRLCAD;
+
+
+BrlMemoryDatabase BrlNewMemoryDatabase(void) {
+    return new MemoryDatabase;
+}
