333f8f985b7543818c0b53f4580e6d05ff12cde2
waleed <iwaleedahmed05@gmail.com>
2026-07-16 12:20:14 +0500

    removed the extra declaration

--- include/brlcad/Database/BagOfTriangles.h
+++ include/brlcad/Database/BagOfTriangles.h
@@ -112,9 +112,6 @@ namespace BRLCAD {
         bool                  FacesHaveNormals(void) const;
         void                  SetFacesHaveNormals(bool facesHaveNormals);
 
-        bool                  FacesHaveThickness(void) const;
-        void                  SetFacesHaveThickness(bool facesHaveThickness);
-
         bool                  UseFaceNormals(void) const;
         void                  SetUseFaceNormals(bool useFaceNormals);
 
